fc6133c520861042ee4c8c45163caeee1ecbfa13
León Serpa <leon.serpa@correo.unimet.edu.ve>
2023-05-26 15:02:55 -0400

    estructura del proyecto

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Classes;
 
-/**
- *
- * @author Windows
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // iniciar menu
+        
+        // crear 2 company simulators (Bugatti y Maserati) con los datos de los trabajadores sacados del txt
+        
     }
     
 }
